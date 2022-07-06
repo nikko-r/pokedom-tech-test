2a882de03ca55f04fb7adca5555781b424ae4e3a
nikko <nikkorivas@outlook.com>
2022-07-06 16:27:54 +0100

    Added basic name search feature

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 import pokemonArray from "./data/pokemon.js"
 console.log(pokemonArray)
 
+const HTMLbody = document.querySelector("body")
 const cardContainer = document.querySelector(".card-container")
 
+// HTMLbody.innerHTML += `
+//     <input id="search-box" type=input name="search" value="Search for a Pokemon">
+//     `
+HTMLbody.insertAdjacentHTML("afterbegin", `
+<input id="search-box" type=input name="search" value="Search for a Pokemon">
+<style>position: absolute; right: 0;</style>
+`)
+
+const searchBox = document.querySelector("#search-box") //Needs to be loaded after its added
+
+//const modifyPokemonEntries = ()
+
 pokemonArray.forEach(pokemonEntry => {
-    //console.log(pokemonEntry.name)
     let types = pokemonEntry.types.toString()
     types = types.replace(",", " & ")
-    console.log(types)
     cardContainer.innerHTML += `
         <div class="card">
             <img class="card__image" src="${pokemonEntry.sprite}" alt="${pokemonEntry.name}">
@@ -18,3 +29,24 @@ pokemonArray.forEach(pokemonEntry => {
         </div>
         `
 })
+
+const filterPokemonByNameEntries = () => {
+    cardContainer.innerHTML = `<div class="card"></div>`
+    pokemonArray.forEach(pokemonEntry => {
+        if (pokemonEntry.name.includes(searchBox.value)) {
+            let types = pokemonEntry.types.toString()
+            types = types.replace(",", " & ")
+            cardContainer.innerHTML += `
+                <div class="card">
+                    <img class="card__image" src="${pokemonEntry.sprite}" alt="${pokemonEntry.name}">
+                    <div class="card__content">
+                        <h1 class="card__heading">${pokemonEntry.name}</h1>
+                        <p class="card__text">${pokemonEntry.name} (#${pokemonEntry.id}) is a ${types} type pokemon.</p>
+                    </div>
+                </div>
+                `
+        }
+    })
+}
+
+searchBox.addEventListener("change", filterPokemonByNameEntries)
