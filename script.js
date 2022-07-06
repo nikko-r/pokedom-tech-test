b0f84d2add76a393851af66289b4beace23b8b11
nikko <nikkorivas@outlook.com>
2022-07-06 15:09:04 +0100

    Fixed types having an & instead of ,

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const cardContainer = document.querySelector(".card-container")
 
 pokemonArray.forEach(pokemonEntry => {
     //console.log(pokemonEntry.name)
-    // const types = pokemonEntry.types.map
-    // console.log(types)
+    let types = pokemonEntry.types.toString()
+    types = types.replace(",", " & ")
+    console.log(types)
     cardContainer.innerHTML += `
         <div class="card">
             <img class="card__image" src="${pokemonEntry.sprite}" alt="${pokemonEntry.name}">
             <div class="card__content">
                 <h1 class="card__heading">${pokemonEntry.name}</h1>
-                <p class="card__text">${pokemonEntry.name} (#${pokemonEntry.id}) is a ${pokemonEntry.types} type pokemon.</p>
+                <p class="card__text">${pokemonEntry.name} (#${pokemonEntry.id}) is a ${types} type pokemon.</p>
             </div>
         </div>
         `
